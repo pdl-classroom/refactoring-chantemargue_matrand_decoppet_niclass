1cc25656e69c3923d19eb05d6f7510e9166dce0e
JorisDecoppetHeigVd <94108834+JorisDecoppetHeigVd@users.noreply.github.com>
2023-06-01 18:05:25 +0200

    edit to use an enum for the size

--- src/main/java/ch/heigvd/pdl/refactoring/OrdersWriter.java
+++ src/main/java/ch/heigvd/pdl/refactoring/OrdersWriter.java
@@ -49,7 +49,7 @@ public class OrdersWriter {
         sb.append(getColorFor(product));
         sb.append("\", ");
 
-        if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
+        if (product.getSize() == Size.Invalid_size) {
             sb.append("\"size\": \"");
             sb.append(getSizeFor(product));
             sb.append("\", ");
---
+++ src/main/java/ch/heigvd/pdl/refactoring/Size.java
@@ -0,0 +1,5 @@
+package ch.heigvd.pdl.refactoring;
+
+public enum Size {
+    XS, S, M, L, XL, XXL, Invalid_size
+}
