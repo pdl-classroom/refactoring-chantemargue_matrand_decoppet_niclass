5e36cbf3ac8519b5696790bd99cb85c74381abad
Dorian <dorian.niclass@heig-vd.ch>
2023-06-08 17:21:42 +0200

    change tests visibiltiy from public to package

--- src/test/java/ch/heigvd/pdl/refactoring/OrdersWriterTest.java
+++ src/test/java/ch/heigvd/pdl/refactoring/OrdersWriterTest.java
@@ -6,28 +6,28 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class OrdersWriterTest {
+class OrdersWriterTest {
     ArrayList<Order> orders = new ArrayList<Order>();
     Order order111 = new Order(111);
 
     @BeforeEach
-    public void SetupOneOrder() {
+    void SetupOneOrder() {
         orders.add(order111);
     }
 
     @Test
-    public void NoOrder() {
+    void NoOrder() {
         assertEquals("{\"orders\": []}", new OrdersWriter(new ArrayList<Order>()).getContents());
     }
 
     @Test
-    public void OneOrder() {
+    void OneOrder() {
         String order111 = "{\"id\": 111, \"products\": []}";
         assertEquals("{\"orders\": [" + order111 + "]}", new OrdersWriter(orders).getContents());
     }
 
     @Test
-    public void TwoOrders() {
+    void TwoOrders() {
         orders.add(new Order(222));
         String order111Json = JsonOrder111WithProduct("");
         String order222Json = "{\"id\": 222, \"products\": []}";
@@ -36,7 +36,7 @@ public class OrdersWriterTest {
     }
 
     @Test
-    public void OneOrderWithOneProduct() {
+    void OneOrderWithOneProduct() {
         order111.addProduct(new Product("Shirt", Color.blue, Size.M, 2.99, "TWD"));
         String order111Json = JsonOrder111WithProduct(
                 "{\"code\": \"Shirt\", \"color\": \"blue\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
@@ -44,7 +44,7 @@ public class OrdersWriterTest {
     }
 
     @Test
-    public void OneOrderWithOneProductNoSize() {
+    void OneOrderWithOneProductNoSize() {
         order111.addProduct(new Product("Pot", Color.red, Size.Invalid_size, 16.50, "SGD"));
         String order111Json = JsonOrder111WithProduct(
                 "{\"code\": \"Pot\", \"color\": \"red\", \"price\": 16.5, \"currency\": \"SGD\"}");
