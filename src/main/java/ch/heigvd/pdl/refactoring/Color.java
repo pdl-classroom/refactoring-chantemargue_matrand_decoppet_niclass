6b9fbb2601275163e74e108793ecd96b7ac7e4ae
Dorian <dorian.niclass@heig-vd.ch>
2023-06-08 17:33:57 +0200

    rename color enum values

--- src/main/java/ch/heigvd/pdl/refactoring/Color.java
+++ src/main/java/ch/heigvd/pdl/refactoring/Color.java
@@ -1,7 +1,7 @@
 package ch.heigvd.pdl.refactoring;
 
 public enum Color {
-   blue,
-   red,
-   yellow
+   BLUE,
+   RED,
+   YELLOW
 }
--- src/test/java/ch/heigvd/pdl/refactoring/OrdersWriterTest.java
+++ src/test/java/ch/heigvd/pdl/refactoring/OrdersWriterTest.java
@@ -37,17 +37,17 @@ public class OrdersWriterTest {
 
     @Test
     public void OneOrderWithOneProduct() {
-        order111.addProduct(new Product("Shirt", Color.blue, Size.M, 2.99, "TWD"));
+        order111.addProduct(new Product("Shirt", Color.BLUE, Size.M, 2.99, "TWD"));
         String order111Json = JsonOrder111WithProduct(
-                "{\"code\": \"Shirt\", \"color\": \"blue\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
+                "{\"code\": \"Shirt\", \"color\": \"BLUE\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
     }
 
     @Test
     public void OneOrderWithOneProductNoSize() {
-        order111.addProduct(new Product("Pot", Color.red, Size.Invalid_size, 16.50, "SGD"));
+        order111.addProduct(new Product("Pot", Color.RED, Size.Invalid_size, 16.50, "SGD"));
         String order111Json = JsonOrder111WithProduct(
-                "{\"code\": \"Pot\", \"color\": \"red\", \"price\": 16.5, \"currency\": \"SGD\"}");
+                "{\"code\": \"Pot\", \"color\": \"RED\", \"price\": 16.5, \"currency\": \"SGD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", new OrdersWriter(orders).getContents());
     }
 
